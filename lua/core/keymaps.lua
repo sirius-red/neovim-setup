f95ca2c4be2fd9011e8604cd9cfdff0a49d1287b
Sirius Red <sirius.red@outlook.com>
2025-03-27 07:20:03 -0300

    Adds an category for AI tools to which key

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,12 @@
 local set = vim.keymap.set
+local options = require('setup').options
 
 --#region AI
 set('i', '<C-;>', function()
 	require('codeium.virtual_text').cycle_or_complete()
 end, { silent = true, expr = true })
+
+if options.copilot then
+	set('n', '<Leader>a', '', { desc = 'AI' }) -- Create the "AI" category in which key
+end
 --#endregion
