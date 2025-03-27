d72ebcf4d22c8ed216c28646138f2587ec63cd6d
Sirius Red <sirius.red@outlook.com>
2025-03-27 03:40:16 -0300

    removes unnecessary comments

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,7 +1,4 @@
--- if true then return {} end -- WARN: COMMENT THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
 
 ---@type LazySpec
 return {
